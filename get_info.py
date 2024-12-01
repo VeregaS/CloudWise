c844043ddf98d059cb039a1ab26d4b15448ab355
VeregaS <sergeiusenkov56@gmail.com>
2024-12-02 02:08:26 +0300

    перед оптимизацией

--- get_info.py
+++ get_info.py
@@ -3,7 +3,9 @@ import aiohttp
 import sqlite3
 from geopy.geocoders import Nominatim
 from geopy.exc import GeopyError
-from datetime import datetime, timedelta
+from datetime import datetime
+
+
 
 
 # Функция отвечает за запись данных погоды в базу данных
@@ -24,7 +26,7 @@ async def city_valid(city_name: str) -> bool:
     geolocator = Nominatim(user_agent="geoapi")
     location = geolocator.geocode(city_name, exactly_one=True, addressdetails=True)
     address = location.raw.get("address", {})
-    found_name = address.get('city') or address.get('town') or address.get('village')
+    found_name = address.get('city') or address.get('town') or address.get('village') or address.get('state')
     if found_name and found_name.lower() == city_name.lower():
         return True
     else:
@@ -35,7 +37,8 @@ async def date_valid(start_date: str, end_date: str) -> bool:
     try:
         start_date = datetime.strptime(start_date, "%Y-%m-%d")
         end_date= datetime.strptime(end_date, "%Y-%m-%d")
-        if end_date > datetime.now() or start_date > end_date:
+        delta = end_date - start_date
+        if end_date > datetime.now() or start_date > end_date or delta.days > 50:
             return False
         else:
             return True
@@ -44,55 +47,49 @@ async def date_valid(start_date: str, end_date: str) -> bool:
 
 
 # получает информацию о погоде в городе
-async def get_weather(city_name: str, start_day: str, end_day: str) -> None:
-    print('все гуд!')
-    # geolocator = Nominatim(user_agent="geoapi")
-    # link = "https://archive-api.open-meteo.com/v1/archive"
-    # location = geolocator.geocode(city_name, exactly_one=True, addressdetails=True)
-    # address = location.raw.get("address", {})
-    # found_name = address.get('city') or address.get('town') or address.get('village')
-    # if found_name.lower() == city_name.lower():
-    #     x, y = location.latitude, location.longitude
-    #     params = {
-    #         "latitude": location.latitude,
-    #         "longitude": location.longitude,
-    #         "start_date": start_day,
-    #         "end_date": end_day,
-    #         "daily": ["temperature_2m_max", "temperature_2m_min"], # запрашиваем дневную информацию о максимальной и минимальной температуре
-    #         "hourly": ["temperature_2m", "apparent_temperature", 
-    #                     "wind_speed_10m", "relative_humidity_2m"], # запрашиваем часовую информацию о действ.температуре, чувств.температуре, скорости ветра и влажности
-    #         "timezone": "Europe/Moscow"
-    #     }
-    #     async with aiohttp.ClientSession() as session:
-    #         async with session.get(link, params=params) as resp:
-    #             output = await resp.json()
-    #             data = []
-    #             for i in range(31):
-    #                 date = output['daily']['time'][i]
-    #                 t_min = output['daily']['temperature_2m_min'][i]
-    #                 t_max = output['daily']['temperature_2m_max'][i]
-    #                 t_day, t_app_day, w_speed_day, rel_humid_day = [], [], [], []
-    #                 for j in range(24):
-    #                     index = j + i * 24
-    #                     day_hour = output['hourly']['time'][index]
-    #                     if date in day_hour:
-    #                         t_hour = output['hourly']['temperature_2m'][index]
-    #                         t_app_hour = output['hourly']['apparent_temperature'][index]
-    #                         w_speed_hour = output['hourly']['wind_speed_10m'][index]
-    #                         rel_humid_hour = output['hourly']['relative_humidity_2m'][index]
-    #                         t_day.append(t_hour)
-    #                         t_app_day.append(t_app_hour)
-    #                         w_speed_day.append(w_speed_hour)
-    #                         rel_humid_day.append(rel_humid_hour)
-    #                 data.append([date, t_min, t_max, str(t_day), str(t_app_day), str(w_speed_day), str(rel_humid_day)])
-    #             return data
-    # else:
-    #     return 'Такого города нет'
+async def get_weather(city_name: str, start_date: str, end_date: str) -> list:
+    geolocator = Nominatim(user_agent="geoapi")
+    link = "https://archive-api.open-meteo.com/v1/archive"
+    location = geolocator.geocode(city_name, exactly_one=True, addressdetails=True)
+    days_count = datetime.strptime(end_date, "%Y-%m-%d") - datetime.strptime(start_date, "%Y-%m-%d") 
+    params = {
+        "latitude": location.latitude,
+        "longitude": location.longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "daily": ["temperature_2m_max", "temperature_2m_min"], # запрашиваем дневную информацию о максимальной и минимальной температуре
+        "hourly": ["temperature_2m", "apparent_temperature", 
+                    "wind_speed_10m", "relative_humidity_2m"], # запрашиваем часовую информацию о действ.температуре, чувств.температуре, скорости ветра и влажности
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link, params=params) as resp:
+            output = await resp.json()
+            data = []
+            for i in range(days_count.days + 1):
+                date = output['daily']['time'][i]
+                t_min = output['daily']['temperature_2m_min'][i]
+                t_max = output['daily']['temperature_2m_max'][i]
+                t_day, t_app_day, w_speed_day, rel_humid_day = [], [], [], []
+                for j in range(24):
+                    index = j + i * 24
+                    day_hour = output['hourly']['time'][index]
+                    if date in day_hour:
+                        t_hour = output['hourly']['temperature_2m'][index]
+                        t_app_hour = output['hourly']['apparent_temperature'][index]
+                        w_speed_hour = output['hourly']['wind_speed_10m'][index]
+                        rel_humid_hour = output['hourly']['relative_humidity_2m'][index]
+                        t_day.append(t_hour)
+                        t_app_day.append(t_app_hour)
+                        w_speed_day.append(w_speed_hour)
+                        rel_humid_day.append(rel_humid_hour)
+                data.append([date, t_min, t_max, str(t_day), str(t_app_day), str(w_speed_day), str(rel_humid_day)])
+            return data
+        
 
 async def get_info_main(city_name: str, start_day: str, end_day: str) -> bool:
-    data = await get_weather(city_name, start_day, end_day)
-    return data
-
-
-if __name__ == "__main__":
-    asyncio.run(get_info_main())
+    try:
+        data = await get_weather(city_name, start_day, end_day)
+        await data_write(data, len(data))
+        return True
+    except Exception as e:
+        return False
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import asyncio
+import time
 from get_info import get_info_main, city_valid, date_valid
 
 async def main():
     while True:
         city = input('Введите город: ')
+        start_time = time.time()
         if await city_valid(city):
-            start_date = input('Вводите в формате год-месяц-дата! \nС какой даты: ')
-            end_date = input('До какой даты: ')
+            print('-' * 44 + '\n', time.time() - start_time, '\t Валидация города\n', '-' * 44 + '\n')
+            start_date = input('Вводите в формате: ГОД-МЕСЯЦ-ДАТА! \nС какой даты: ')
+            end_date = input('До какой даты (не больше 50 дней дельта): ')
+            start_time = time.time()
             if await date_valid(start_date, end_date):
+                print('-' * 44 + '\n', time.time() - start_time, '\t Валидация даты\n', '-' * 44 + '\n')
                 break
             else:
-                print('Неверные даты! Попробуйте снова.')
+                print('Неверные даты! Попробуйте снова и вводите дату в нужном формате.')
         else:
             print('Такого города нет! Попробуйте снова.')
-    ans = await get_info_main(city, start_date, end_date)
-    return ans
+    start_time = time.time()
+    status = await get_info_main(city, start_date, end_date)
+    print('-' * 44 + '\n', time.time() - start_time, '\t Получение инфорации\n', '-' * 44 + '\n')
+    if status:
+        print('Все гуд!')
+    else:
+        print('Произошла ошибка! Попробуйте снова.')
+    
 
 
 if __name__ == "__main__":
